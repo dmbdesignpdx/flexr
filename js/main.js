ca80ee8ca68e55889c011d29c312c31d7b0b2a42
Daniel Blake <dmblakedesign@gmail.com>
2016-07-22 19:44:57 -0700

    updated b3.0.0

--- js/main.js
+++ js/main.js
@@ -1,1 +1,136 @@
-var scrolled,nav=document.getElementById("nav"),menu=document.getElementById("menu"),menuLinks=document.getElementsByTagName("li"),menuShowing;window.onscroll=function(){window.innerWidth>740?window.pageYOffset>=60&&!scrolled?(scrolled=!0,nav.classList.add("shrink-large")):window.pageYOffset<60&&scrolled&&(scrolled=!1,nav.classList.remove("shrink-large")):window.pageYOffset>=20&&!scrolled?(scrolled=!0,nav.classList.add("shrink-small")):window.pageYOffset<20&&scrolled&&(scrolled=!1,nav.classList.remove("shrink-small"))},window.onresize=function(){window.innerWidth<740&&scrolled?(nav.classList.remove("shrink-large"),nav.classList.add("shrink-small")):window.innerWidth>740&&scrolled&&(nav.classList.add("shrink-large"),nav.classList.remove("shrink-small"))},document.getElementById("poma").onclick=function(n){n.stopPropagation(),menuShowing=!0,menu.classList.add("show-menu")},menu.onclick=function(n){n.stopPropagation()},document.body.onclick=function(){menuShowing&&(menuShowing=!1,menu.classList.remove("show-menu"))};for(var i=0;i<menuLinks.length;i++)menuLinks[i].onclick=function(){menuShowing&&(menuShowing=!1,menu.classList.remove("show-menu"))};
+// For Flexr Grid - Beta 3.0.0
+
+var scrolled,
+nav = document.getElementById('nav'),
+menu = document.getElementById('menu'),
+menuLinks = document.getElementsByTagName('li'),
+menuShowing;
+
+
+// scrolling
+
+window.onscroll = function() {
+
+    if (window.innerWidth > 740){
+
+        if (window.pageYOffset >= 60 && !scrolled){
+
+            scrolled = true;
+
+            nav.classList.add('shrink-large')
+
+        }
+
+        else if (window.pageYOffset < 60 && scrolled) {
+
+            scrolled = false;
+
+            nav.classList.remove('shrink-large');
+
+        }
+
+    }
+
+    else {
+
+        if (window.pageYOffset >= 20 && !scrolled){
+
+            scrolled = true;
+
+            nav.classList.add('shrink-small')
+
+        }
+
+        else if (window.pageYOffset < 20 && scrolled) {
+
+            scrolled = false;
+
+            nav.classList.remove('shrink-small')
+
+        }
+
+    }
+
+}
+
+
+// Resizing
+
+window.onresize = function() {
+
+    if(window.innerWidth < 740 && scrolled) {
+
+        nav.classList.remove('shrink-large');
+
+        nav.classList.add('shrink-small')
+
+    }
+
+    else if (window.innerWidth > 740 && scrolled) {
+
+        nav.classList.add('shrink-large');
+
+        nav.classList.remove('shrink-small')
+
+    }
+
+}
+
+// window.onload = function() {
+//
+//     if(window.innerWidth > 740 && window.pageYOffset > 20) {
+//
+//         nav.classList.add('shrink-large');
+//
+//         nav.classList.add('shrink-small')
+//
+//     }
+//
+// }
+
+
+// pullout menu
+
+document.getElementById('poma').onclick = function(event) {
+
+    event.stopPropagation();
+
+    menuShowing = true;
+
+    menu.classList.add('show-menu')
+
+}
+
+menu.onclick = function(event) {
+
+    event.stopPropagation()
+
+}
+
+document.body.onclick = function() {
+
+    if (menuShowing) {
+
+        menuShowing = false;
+
+        menu.classList.remove('show-menu')
+
+    }
+
+}
+
+for (var i = 0; i < menuLinks.length; i++) {
+
+    menuLinks[i].onclick = function() {
+
+        if (menuShowing) {
+
+            menuShowing = false;
+
+            menu.classList.remove('show-menu')
+
+        }
+
+    }
+
+}
