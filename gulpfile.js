52b600f8a45b79c89275991e7b7d5b90667f721d
dmbdesignpdx <dmblakedesign@gmail.com>
2017-07-09 16:42:52 -0700

    adds gulp-plumber

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,15 @@ const gulp = require('gulp'),
 babel = require('gulp-babel'),
 uglify = require('gulp-uglify'),
 concat = require('gulp-concat'),
-shell = require('gulp-shell');
+shell = require('gulp-shell'),
+plumber = require('gulp-plumber'),
+port = 4000;
 
 gulp.task('scripts', () => {
   gulp.src(['./js/*.js' ])
+    .pipe(plumber())
     .pipe(concat('flexr.min.js'))
-    .pipe(babel({
-      presets: ['es2015']
-    }))
+    .pipe(babel({presets: ['es2015']}))
     .pipe(uglify())
     .pipe(gulp.dest('./dist/'))
     .pipe(gulp.dest('./_site/dist/'));
@@ -17,6 +18,8 @@ gulp.task('scripts', () => {
 
 gulp.task('watch', () => gulp.watch('./js/*.js', ['scripts']));
 
-gulp.task('serve', shell.task(['jekyll serve']));
+gulp.task('serve', shell.task('jekyll serve'));
+
+gulp.task('kill', shell.task(`kill -9 $(lsof -t -i:${port})`));
 
 gulp.task('default', ['serve', 'scripts', 'watch']);
