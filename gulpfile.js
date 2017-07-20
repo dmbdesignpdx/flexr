83b20b5383c03110b8061b3b5b700bf2e6557452
dmbdesignpdx <daniel@danielblake.design>
2017-07-20 12:18:22 -0700

    adds more icons for browsers

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,43 @@ uglify = require('gulp-uglify'),
 concat = require('gulp-concat'),
 shell = require('gulp-shell'),
 plumber = require('gulp-plumber'),
-port = 4000;
+browserSync = require('browser-sync').create();
 
 gulp.task('scripts', () => {
-  gulp.src(['./js/*.js' ])
-    .pipe(plumber())
-    .pipe(concat('flexr.min.js'))
-    .pipe(babel({presets: ['es2015']}))
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/'))
-    .pipe(gulp.dest('./_site/dist/'));
+   return gulp.src(['./js/*.js' ])
+   .pipe(plumber())
+   .pipe(concat('flexr.min.js'))
+   .pipe(babel({presets: ['es2015']}))
+   .pipe(uglify())
+   .pipe(gulp.dest('./dist/'))
+   .pipe(gulp.dest('./_site/dist/'));
 });
 
-gulp.task('watch', () => gulp.watch('./js/*.js', ['scripts']));
+gulp.task('wait', ['scripts'], done => {
+   done();
+   browserSync.reload();
+});
+
+gulp.task('watch', () => gulp.watch('./js/*.js', ['wait']));
+
+gulp.task('lookout', () => {
+   gulp.watch('./js/*.js', ['wait']);
+   browserSync.watch('./_site/*').on('change', browserSync.reload);
+});
+
+gulp.task('sync', () => {
+   browserSync.init({
+      server: {baseDir:'./_site/'},
+      port: 4000,
+      open: false
+   });
+});
 
 gulp.task('serve', shell.task('jekyll serve'));
 
-gulp.task('kill', shell.task(`kill -9 $(lsof -t -i:${port})`));
+gulp.task('auto', ['serve', 'scripts', 'sync', 'lookout']);
 
 gulp.task('default', ['serve', 'scripts', 'watch']);
+
+// Emergency kill if Plumber fails
+gulp.task('kill', shell.task(`kill -9 $(lsof -t -i:4000)`));
